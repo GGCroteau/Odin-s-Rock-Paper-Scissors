9550d038270aadc52821df2200a39bbdb8b3dcb0
gguimond <guigc92@hotmail.ca>
2026-06-03 17:12:24 -0400

    Added minimalistic UI to play the game and logic to show the score after each button press. At a score of 5 on either side, the game ends and the buttons are removed until the browser reload.

--- script.js
+++ script.js
@@ -13,25 +13,34 @@ function getHumanChoice(){
     return prompt("Choose one: Rock, Paper or Scissor?");    
 }
 
-function humanWinRound(humanChoice, computerChoice, scores){
-    console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+function humanWinRound(humanChoice, computerChoice, scores) {
+    let result = document.querySelector(ROUND_RESULT);
+    result.textContent = `You win! ${humanChoice} beats ${computerChoice}.`;
     return scores.humanScore++;    
 }
 
-function humanLoseRound(humanChoice, computerChoice, scores){
-    console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+function humanLoseRound(humanChoice, computerChoice, scores) {
+    let result = document.querySelector(ROUND_RESULT);
+    result.textContent = `You lose! ${computerChoice} beats ${humanChoice}.`;
     return scores.computerScore++;
 }
 
-function showScore(scores){
-    console.log(`Your score: ${scores.humanScore} \nComputer score: ${scores.computerScore}`)
+function showScore(scores) {
+    let result = document.querySelector(CURRENT_SCORE);
+    if (scores.humanScore >= 5 || scores.computerScore >= 5)        
+        result.textContent = `Final Score: Your score: ${scores.humanScore} Computer score ${scores.computerScore}`;    
+    else
+        result.textContent = `Your score: ${scores.humanScore} \nComputer score: ${scores.computerScore}`;
 }
 
 function playRound(humanChoice, computerChoice, scores){
     humanChoice = humanChoice.toLowerCase();
 
-    if(humanChoice == computerChoice)
-        console.log(`Draw! ${humanChoice} and ${computerChoice}`);
+    if (humanChoice == computerChoice) {
+        let result = document.querySelector(ROUND_RESULT);
+        result.textContent = `Draw! ${humanChoice} and ${computerChoice}`;
+    }
+
     if(humanChoice == "rock" && computerChoice == "paper")
         humanLoseRound(humanChoice, computerChoice, scores);
     if(humanChoice == "rock" && computerChoice == "scissor")
@@ -54,17 +63,27 @@ function playGame(){
         computerScore: 0
     }
     
-    let roundNumber = 1;
-
-    while(roundNumber <= 5)
-    {
-        console.log(`Round ${roundNumber}`);
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection, scores);
-        roundNumber++;
-    }    
-    console.log(`Final Score: Your score: ${scores.humanScore} Computer score ${scores.computerScore}`);
+    const btns = document.querySelectorAll(".selector");
+    for (let btn of btns) {
+        btn.addEventListener("click", () => {
+            playRound(btn.textContent, getComputerChoice(), scores);
+            if (scores.humanScore >= 5 || scores.computerScore >= 5) {
+                document.querySelector("#buttons").remove();
+            }
+        });
+    }
 }
+
+const CURRENT_SCORE = ".current-score";
+const ROUND_RESULT = ".round-result";
+const result = document.getElementById("result")
+const currentScore = document.createElement("div");
+currentScore.classList.add("current-score");
+
+const roundResult = document.createElement("div");
+roundResult.classList.add("round-result");
+
+result.appendChild(roundResult);
+result.appendChild(currentScore);
 
 playGame();
